32b14e8d36ac3b0a3744d7ce3d2c1a9d11656dce
Perryjay30 <pelumijsh@gmail.com>
2023-02-12 23:41:19 -1200

    RegistrationService

---
+++ src/main/java/com/application/perrylogistics/Service/RegistrationService.java
@@ -0,0 +1,8 @@
+package com.application.perrylogistics.Service;
+
+import com.application.perrylogistics.Data.dtos.Request.RegistrationRequest;
+
+public interface RegistrationService {
+
+    String register(RegistrationRequest registrationRequest);
+}
